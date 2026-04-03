bf7a87e14cb357dadf53beb956857c952c6037ef
Mike Curtis <mikecurtis1@gmail.com>
2026-04-03 16:24:43 -0400

    Update index.php
    Added method to read metadata for MARC21 encoding from a csv file submitted via HTML form.

--- public/index.php
+++ public/index.php
@@ -3,6 +3,32 @@
 require_once('../src/MARC21Maker.php');
 require_once('../src/EscapeString.php');
 
+$file = $_FILES['csv']['tmp_name'];
+
+$delimiter = "\t";
+
+$tsv = array();
+
+if (($handle = fopen($file, "r")) !== FALSE) {
+    $headers = fgetcsv($handle, null, $delimiter);
+    while ( $row = fgetcsv($handle, null, $delimiter) ) {
+        $tsv[] = array_combine($headers, $row);
+    }
+}
+
+$set = '';
+				
+foreach ($tsv as $i => $row) {
+	$mrc = new MARC21Maker($row['L5'],$row['L6'],$row['L7']);
+	$mrc->addControlField('001',$row['number']);
+	$mrc->addDataField('100','0',' ','$a' . $row['author']);
+	$mrc->addDataField('245',' ',' ','$a' . $row['title']);
+	$mrc->addDataField('264',' ','1','$c' . $row['pubdate']);
+	$set .= $mrc->getMRC();
+}
+
+$mrc->emitMRC('utf-8',$set,'set_of_marc_records');
+
 /*
 $mrc = new MARC21Maker('n','g','m');
 $mrc->addControlField('001','123456789');
@@ -15,6 +41,7 @@ $data = $mrc->getMRC();
 $mrc->emitMRC('utf-8',$data,'Seven_Samurai');
 */
 
+/*
 $set = '';
 
 $mrc = new MARC21Maker('n','a','m');
@@ -28,16 +55,16 @@ $mrc = new MARC21Maker('n','a','m');
 $mrc->addControlField('001','0002');
 $mrc->addDataField('100','0',' ','$aJohn Doe');
 $mrc->addDataField('245',' ',' ','$aTitle #2');
-$mrc->addDataField('264',' ','1','$c2025');
+$mrc->addDataField('264',' ','1','$c2024');
 $set .= $mrc->getMRC();
 
 $mrc = new MARC21Maker('n','a','m');
 $mrc->addControlField('001','0003');
 $mrc->addDataField('100','0',' ','$aJohn Doe');
 $mrc->addDataField('245',' ',' ','$aTitle #3');
-$mrc->addDataField('264',' ','1','$c2026');
+$mrc->addDataField('264',' ','1','$c2024');
 $set .= $mrc->getMRC();
 
 $mrc->emitMRC('utf-8',$set,'set_of_marc_records');
-
+*/
 ?>
